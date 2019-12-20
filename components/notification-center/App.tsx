43d717fe3721b79955682c046c8591ea55671eea
Chris Watson <cwatson1988@gmail.com>
2019-12-20 14:54:21 +0000

    add types for filtering

--- components/notification-center/App.tsx
+++ components/notification-center/App.tsx
@@ -4,15 +4,21 @@ import NotificationCenter from "./components/NotificationCenter";
 import FilterPanel from "./components/FilterPanel";
 import NotificationsPanel from "./components/NotificationsPanel";
 import NotificationDetailPanel from "./components/NotificationDetailPanel";
+import INotification from "../../types/Notification-definitions/INotification";
 
 const App = (): React.ReactElement => {
   const { notifications } = useNotifications();
+
+  const types: string[] = Array.from(
+    new Set(notifications.map((item: INotification) => item.type))
+  );
+
   return (
     <div id="app">
       <NotificationCenter title="Notification Center">
-        <FilterPanel />
+        <FilterPanel types={types} />
         <NotificationsPanel notifications={notifications} />
-        <NotificationDetailPanel notification />
+        <NotificationDetailPanel />
       </NotificationCenter>
     </div>
   );

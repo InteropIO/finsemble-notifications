74396a15d42f2e961111f68d681436be7d7ef672
Chris Watson <cwatson1988@gmail.com>
2020-01-10 18:46:07 +0000

    toasts are now positioned correctly and have a better sizing.

--- components/notification-toasts/App.tsx
+++ components/notification-toasts/App.tsx
@@ -7,27 +7,27 @@ import INotification from "../../types/Notification-definitions/INotification";
 import Animate from "../shared/components/Animate";
 
 function App(): React.ReactElement {
-  const { notifications, doAction } = useNotifications();
-  return (
-    <StoreProvider>
-      <Drawer notifications={notifications}>
-        {notifications &&
-          notifications.map((notification: INotification) => (
-            <Animate
-              displayDuration={2000}
-              animateIn="slide-in-fwd-bottom"
-              animateOut="slide-out-right"
-            >
-              <Notification
-                key={notification.id}
-                notification={notification}
-                doAction={doAction}
-              ></Notification>
-            </Animate>
-          ))}
-      </Drawer>
-    </StoreProvider>
-  );
+	const { notifications, doAction } = useNotifications();
+	return (
+		<StoreProvider>
+			<Drawer notifications={notifications}>
+				{notifications &&
+					notifications.map((notification: INotification) => (
+						<Animate
+							displayDuration={3000}
+							animateIn="slide-in-fwd-bottom"
+							animateOut="slide-out-right"
+						>
+							<Notification
+								key={notification.id}
+								notification={notification}
+								doAction={doAction}
+							></Notification>
+						</Animate>
+					))}
+			</Drawer>
+		</StoreProvider>
+	);
 }
 
 export default App;
--- components/notification-toasts/components/Drawer.tsx
+++ components/notification-toasts/components/Drawer.tsx
@@ -5,32 +5,32 @@ import INotification from "../../../types/Notification-definitions/INotification
 const { useEffect, useState, useContext, useRef } = React;
 
 interface Props {
-  children: React.PropsWithChildren<any>;
-  notifications?: INotification[];
+	children: React.PropsWithChildren<any>;
+	notifications?: INotification[];
 }
 
 function Drawer(props: Props): React.ReactElement {
-  // const { setWindowId, setWindowPosition } = useFinsemble();
-  const { setNotificationDrawerPosition } = useNotifications();
-  const inputEl = useRef(null);
-  const { notifications } = props;
+	// const { setWindowId, setWindowPosition } = useFinsemble();
+	const { setNotificationDrawerPosition } = useNotifications();
+	const inputEl = useRef(null);
+	const { notifications } = props;
 
-  useEffect(() => {
-    console.log("#run");
-    inputEl.current.getBoundingClientRect().height;
-    // TODO: replace this for spawn params passed by the client
-    setNotificationDrawerPosition(inputEl, {
-      bottom: 0,
-      left: 100,
-      monitor: "primary"
-    });
-  }, [notifications, setNotificationDrawerPosition]);
+	useEffect(() => {
+		console.log("#run");
+		inputEl.current.getBoundingClientRect().height;
+		// TODO: replace this for spawn params passed by the client
+		setNotificationDrawerPosition(inputEl, {
+			bottom: 0,
+			right: 0,
+			monitor: "primary"
+		});
+	}, [notifications, setNotificationDrawerPosition]);
 
-  return (
-    <div id="toasts-drawer" ref={inputEl}>
-      {props.children}
-    </div>
-  );
+	return (
+		<div id="toasts-drawer" ref={inputEl}>
+			{props.children}
+		</div>
+	);
 }
 
 export default Drawer;

7e176abf7d76a9bc712b92167a531edcb90dea11
Chris Watson <cwatson1988@gmail.com>
2020-01-10 18:42:58 +0000

    Fix to notification visuals

--- components/notification-drawer/App.tsx
+++ components/notification-drawer/App.tsx
@@ -8,32 +8,30 @@ import { useState } from "react";
 import Animate from "../shared/components/Animate";
 
 function App(): React.ReactElement {
-  const { notifications, doAction } = useNotifications();
-  return (
-    <StoreProvider>
-      <Drawer>
-        {notifications &&
-          notifications.map(
-            (notification: INotification) =>
-              !notification.isSnoozed &&
-              !notification.isActionPerformed && (
-                <Animate
-                  // TODO: this needs a better key to differentiate when notification is updated
-                  key={notification.id}
-                  animateIn="slide-in-fwd-bottom"
-                  animateOut="slide-out-right"
-                >
-                  <Notification
-                    key={notification.id}
-                    notification={notification}
-                    doAction={doAction}
-                  ></Notification>
-                </Animate>
-              )
-          )}
-      </Drawer>
-    </StoreProvider>
-  );
+	const { notifications, doAction } = useNotifications();
+	return (
+		<Drawer>
+			{notifications &&
+				notifications.map(
+					(notification: INotification) =>
+						!notification.isSnoozed &&
+						!notification.isActionPerformed && (
+							<Animate
+								// TODO: this needs a better key to differentiate when notification is updated
+								key={notification.id}
+								animateIn="slide-in-fwd-bottom"
+								animateOut="slide-out-right"
+							>
+								<Notification
+									key={notification.id}
+									notification={notification}
+									doAction={doAction}
+								></Notification>
+							</Animate>
+						)
+				)}
+		</Drawer>
+	);
 }
 
 export default App;

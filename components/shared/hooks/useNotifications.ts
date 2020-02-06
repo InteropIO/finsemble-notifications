56f32b3de144138bf574037ab57cf35acc59f20e
Chris Watson <47246322+WatsonCIQ@users.noreply.github.com>
2020-02-06 20:20:37 +0000

    Update components/shared/hooks/useNotifications.ts

--- components/shared/hooks/useNotifications.ts
+++ components/shared/hooks/useNotifications.ts
@@ -203,7 +203,7 @@ export default function useNotifications() {
 	 * @param filter
 	 */
 	const getNotificationHistory = (
-		since: string = "2000-01-01T00:00:00.000Z",
+		since: string = "1969-12-31T23:59:59.999Z",
 		filter: null | IFilter = null
 	): Promise<INotification[]> => {
 		NOTIFICATION_CLIENT = new NotificationClient();

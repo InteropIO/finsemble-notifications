69988697ed687466267a06dc871b789b0aee0da7
WatsonCIQ <chris.watson@chartiq.com>
2020-02-18 11:05:20 +0000

    small comments and todo

--- tests/services/ui.spec.tsx
+++ tests/services/ui.spec.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { expect } from "chai";
 import { render } from "@testing-library/react";
 // import "@testing-library/jest-dom/extend-expect";
+// TODO: this component needs to be cleaned up
 import NotificationIcon from "../../components/notification-icon/NotificationIcon";
 
 // @ts-ignore
@@ -9,6 +10,7 @@ describe("testing the UI", () => {
 	// @ts-ignore
 	it("loads and displays greeting", () => {
 		// @ts-ignore
+		// example but needs to be changed from NotificationIcon
 		const { container, getByText } = render(<NotificationIcon />);
 		// @ts-ignore
 		expect(getByText("hello world")).to.be.true();

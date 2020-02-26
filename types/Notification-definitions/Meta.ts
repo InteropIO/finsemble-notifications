5869c12a6a4de13c9d561dcc662dc48b9d651eec
Gerard Petersen <gerard@chartiq.com>
2020-02-26 16:15:56 +0000

    Moving cssClass and notification sound

--- types/Notification-definitions/Meta.ts
+++ types/Notification-definitions/Meta.ts
@@ -4,18 +4,9 @@
  */
 export default class Meta {
 	/**
-	 * An array of objects. If more than one object is used it will use an OR match.
-	 *
-	 * @name Meta#cssClassName
-	 * @type string
-	 */
-	cssClassName?: string;
-	notificationAlertSound?: string;
-
-	/**
 	 * Allows for putting any key that is a string onto this object.
 	 *
 	 * @type any
 	 */
-	[key: string]: any|{};
+	[key: string]: any | {};
 }

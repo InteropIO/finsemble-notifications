eca412544d27a45b8e17c39421af4d7858b15053
WatsonCIQ <chris.watson@chartiq.com>
2020-02-26 09:45:29 +0000

    remove hardcoded values part 1

--- components/notification-toasts/App.tsx
+++ components/notification-toasts/App.tsx
@@ -4,6 +4,9 @@ import Notification from "../shared/components/Notification";
 import useNotifications from "../shared/hooks/useNotifications";
 import INotification from "../../types/Notification-definitions/INotification";
 import Animate from "../shared/components/Animate";
+import { SpawnParams } from "../../types/FSBL-definitions/services/window/Launcher/launcher";
+/* eslint-disable @typescript-eslint/no-var-requires */
+const _get = require("lodash.get");
 
 const { useEffect, useState } = React;
 
@@ -16,15 +19,14 @@ function App(): React.ReactElement {
 		(async () => setConfig(await getNotificationConfig("notification-toasts")))();
 	}, [getNotificationConfig]);
 
+	const windowShowParams: SpawnParams = _get(config, "config.position", {
+		bottom: 0,
+		right: 0,
+		monitor: 0
+	});
+
 	return (
-		<Drawer
-			notifications={notifications}
-			windowShowParams={{
-				bottom: 0,
-				right: 0,
-				monitor: 0
-			}}
-		>
+		<Drawer notifications={notifications} windowShowParams={windowShowParams}>
 			{config &&
 				notifications &&
 				notifications.map(

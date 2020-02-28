47d7896efc183b394193bb3cc34d1ae1bf46e303
WatsonCIQ <chris.watson@chartiq.com>
2020-02-28 03:48:00 +0000

    Removed Filter component until it's ready.

--- components/notification-center/App.tsx
+++ components/notification-center/App.tsx
@@ -1,22 +1,17 @@
 import * as React from "react";
 import useNotifications from "../shared/hooks/useNotifications";
 import NotificationCenter from "./components/NotificationCenter";
-import FilterPanel from "./components/FilterPanel";
 import NotificationsPanel from "./components/NotificationsPanel";
 import NotificationDetailPanel from "./components/NotificationDetailPanel";
-import INotification from "../../types/Notification-definitions/INotification";
 import { useState } from "react";
 
 const App = (): React.ReactElement => {
 	const { notifications, doAction } = useNotifications();
 	const [activeNotification, setActiveNotification] = useState();
 
-	const types: string[] = Array.from(new Set(notifications.map((item: INotification) => item.type)));
-
 	return (
 		<div id="app">
 			<NotificationCenter title="Notification Center">
-				{/* <FilterPanel types={types} /> */}
 				<div id="main-content">
 					{notifications.length === 0 ? (
 						// TODO:Add animated no notification component

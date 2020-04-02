945884f861390aabcc4c2bec2c324555d781d83e
Kris West <kris@chartiq.com>
2020-04-02 16:34:14 +0100

    Fixes based on Andy's feedback

--- dot-net-notifications/FinsembleNotifications/ActionTypes.cs
+++ dot-net-notifications/FinsembleNotifications/ActionTypes.cs
@@ -4,11 +4,11 @@ namespace ChartIQ.Finsemble.Notifications
 {
 	public class ActionTypes
 	{
-		public static String DISMISS = "DISMISS";
-		public static String SNOOZE = "SNOOZE";
-		public static String SPAWN = "SPAWN";
-		public static String QUERY = "QUERY";
-		public static String PUBLISH = "PUBLISH";
-		public static String TRANSMIT = "TRANSMIT";
+		public const String DISMISS = "DISMISS";
+		public const String SNOOZE = "SNOOZE";
+		public const String SPAWN = "SPAWN";
+		public const String QUERY = "QUERY";
+		public const String PUBLISH = "PUBLISH";
+		public const String TRANSMIT = "TRANSMIT";
 	}
 }

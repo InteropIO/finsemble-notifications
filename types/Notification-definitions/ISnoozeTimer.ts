58a3ad6941bb538cb67dfb881db8e2b8cd4b0f74
Gerard Petersen <gerard@chartiq.com>
2020-05-27 18:28:02 +0100

    Can now wake and resnooze from boot

--- types/Notification-definitions/ISnoozeTimer.ts
+++ types/Notification-definitions/ISnoozeTimer.ts
@@ -2,11 +2,13 @@
  * @property {number} timeoutId - The timeoutId returned when calling setTimout
  * @property {string} notificationId - The id of the notification being snoozed
  * @property {number} snoozeInterval - How long the is meant to be snooze for.
+ * @property {number} wakeMilliseconds - The number of milliseconds after epoch that the timer should wake
  */
 import Timeout = NodeJS.Timeout;
 
 export default interface ISnoozeTimer {
 	timeoutId: Timeout;
 	notificationId: string;
+	wakeEpochMilliseconds: number;
 	snoozeInterval: number;
 }

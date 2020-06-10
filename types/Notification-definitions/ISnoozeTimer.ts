5872955f55d8a42a9af449f51f9b0cb6008fc041
gerard-ciq <50723962+gerard-ciq@users.noreply.github.com>
2020-06-10 14:25:23 +0100

    fix comment rename
    Co-authored-by: Siddharth Dalal <sgd2z@users.noreply.github.com>

--- types/Notification-definitions/ISnoozeTimer.ts
+++ types/Notification-definitions/ISnoozeTimer.ts
@@ -2,7 +2,8 @@
  * @property {number} timeoutId - The timeoutId returned when calling setTimout
  * @property {string} notificationId - The id of the notification being snoozed
  * @property {number} snoozeInterval - How long the is meant to be snooze for.
- * @property {number} wakeMilliseconds - The number of milliseconds after epoch that the timer should wake
+ * @property {number} wakeEpochMilliseconds - The number of milliseconds after epoch that the timer should wake
+
  */
 import Timeout = NodeJS.Timeout;
 

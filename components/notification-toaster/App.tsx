cda531bf878d78fba6fa6a440c823f0711596dc2
gerard-ciq <50723962+gerard-ciq@users.noreply.github.com>
2020-06-15 17:58:27 +0100

    Toaster now shows the correct count on boot (#65)
    https://chartiq.kanbanize.com/ctrl_board/106/cards/26329/details/
    Notifications - Toaster correct count on boot
    
    - [ ] Run this branch
    - [ ] Send a notification
    - [ ] Notice the number on the toaster has increased
    - [ ] Restart Finsemble 
    - [ ] Notice the number on the toaster is the same

--- components/notification-toaster/App.tsx
+++ components/notification-toaster/App.tsx
@@ -10,14 +10,18 @@ import { usePubSub } from "../shared/hooks/finsemble-hooks";
 const { useEffect, useState } = React;
 
 function App(): React.ReactElement {
-	const { notifications, activeNotifications } = useNotifications();
+	const { notifications, activeNotifications } = useNotifications({ config: { notificationsHistory: true } });
 	const pubSubTopic = "notification-ui";
 	const [notificationSubscribeMessage, notificationsPublish] = usePubSub(pubSubTopic);
-	const [currentMonitor, setCurrentMonitor] = useState(null);
+	const [currentMonitor] = useState(null);
+	const [count, setCount] = useState(activeNotifications(notifications).length);
+
+	useEffect(() => {
+		setCount(activeNotifications(notifications).length);
+	}, [notifications]);
 
 	useEffect(() => {
 		const hotkey = _get(FSBL.Clients.WindowClient.getSpawnData(), "notifications.hotkey", null);
-		console.log(hotkey);
 		// TODO: Pull this out of the component into a hook
 		if (hotkey) {
 			FSBL.Clients.HotkeyClient.addGlobalHotkey(hotkey, () => {
@@ -104,9 +108,7 @@ function App(): React.ReactElement {
 			<div onMouseDown={onmousedown} onMouseUp={onmouseup} className="drag-container">
 				<DragHandleIcon id="drag-area" className="drag-area" />
 			</div>
-			{activeNotifications(notifications).length > 0 && (
-				<div id="notification-number">{activeNotifications(notifications).length}</div>
-			)}
+			{count > 0 && <div id="notification-number">{count}</div>}
 			<NotificationIcon
 				className={notificationSubscribeMessage.showDrawer ? "toaster-icons--active" : "toaster-icons"}
 				onClick={() => toggleDrawer()}
